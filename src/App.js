95401ec9094cc72a4298c5a5bafa2e9f26a74d0e
Jobi Mathai <jobi.mathai@gmail.com>
2019-02-03 00:42:02 -0800

    moved location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'characters' from '../characters.json';
+import 'characters' from './characters.json';
 
 class App extends Component {
   render() {
