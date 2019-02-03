ea610635c6e67b95d431147fbbaafe29d126a716
Jobi Mathai <jobi.mathai@gmail.com>
2019-02-03 00:38:44 -0800

    added characters to the game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'characters' from '../characters.json';
 
 class App extends Component {
   render() {
