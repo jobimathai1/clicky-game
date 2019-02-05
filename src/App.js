658876e9e7e9b23ab5a152e454b30c0cfd0fbaa1
Jobi Mathai <jobi.mathai@gmail.com>
2019-02-04 23:25:57 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'characters' from './characters.json';
+import characters from './characters.json';
+import CharacterCard from './components/CharacterCard';
+import Nav from './components/Nav';
+import Title from './components/Title';
+
+function moveCharacters(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
 
 class App extends Component {
+
+  state = {
+  characters,
+  currentScore: 0,
+  topScore: 0,
+  rightWrong: "",
+  clicked: [],
+};
+
+
   render() {
     return (
       <div className="App">
